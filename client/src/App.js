2be7f60941d547e99a2d59bade3891b6893b89b0
Jose Andrade <jose.andrade2@thermofisher.com>
2019-11-17 15:44:42 -0800

    my change in app

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ function App() {
     <div className="App">
       <header >
         <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">Fib calculator</h1>
       </header>
       <div>
         <Link to="/">Home</Link>/
